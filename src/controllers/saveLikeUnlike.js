ee7595e1057384fd2c93592ecd71310aae91f7a4
Aditi Verma <aditiverma1009@gmail.com>
2022-12-27 16:55:40 +0530

    update route for like and unlike

--- src/controllers/saveLikeUnlike.js
+++ src/controllers/saveLikeUnlike.js
@@ -1,20 +1,19 @@
 const Model = require('../../models');
 
-function saveLikeUnlikeController(bookId, likeUnlike) {
+const saveLikeUnlikeController = async (bookId, likeUnlike) => {
   // check if book exists in db - if doesn't send book not found
   try {
-    return Model.Books.update({
+    await Model.Books.update({
       like_unlike: likeUnlike
     }, {
       where: {
         book_id: bookId
       }
-    }).then(() => {
-      return 'Success!';
     });
+    return 'Success!';
   } catch (err) {
     throw new Error('Could not save the like/unlike');
   }
-}
+};
 
 module.exports = { saveLikeUnlikeController };
--- src/routes/likeUnlike.js
+++ src/routes/likeUnlike.js
@@ -5,10 +5,9 @@ const likeUnlikeRouter = Router();
 
 likeUnlikeRouter.post('/book/:book_id/like/:like', ({ params }, response) => {
   try {
-    console.log(params);
-    const result = saveLikeUnlikeController(params.book_id, (Boolean)(params.like));
-    console.log(result);
-    response.status(200).send('Like/Unlike', result);
+    saveLikeUnlikeController(params.book_id, params.like === 'true').then((result) => {
+      response.status(200).send(result);
+    });
   } catch (err) {
     response.status(500).send('Something broke!');
   }
